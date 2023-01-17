ea8c1cf3694a64e2167652628c428334546caaf6
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-17 20:55:37 +0900

    refactor: 상수의 값을 다른 상수를 재사용하도록 리팩터링

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,13 @@
 export const THUMBNAIL_WIDTH = 1600;
 export const THUMBNAIL_HEIGHT = 900;
 
-export const START_X = 0;
-export const START_Y = 0;
-export const RGBA_ARRAY_SIZE = 4;
-
 export const TRIANGLE_WIDTH = 40;
-export const TRIANGLE_GAP = 40;
+export const TRIANGLE_GAP = TRIANGLE_WIDTH / 2;
 export const TRIANGLE_MAX_HEIGHT = 3;
 export const TRIANGLE_MIN_HEIGHT = 1;
-export const TRIANGLE_COUNT = 850;
+export const TRIANGLE_COUNT =
+  (THUMBNAIL_WIDTH / TRIANGLE_WIDTH) * (THUMBNAIL_HEIGHT / TRIANGLE_WIDTH) * 2;
+export const TRIANGLE_DRAW_START_X = -TRIANGLE_WIDTH;
+export const TRIANGLE_DRAW_START_Y = -TRIANGLE_WIDTH;
+
+export const RGBA_ARRAY_SIZE = 4;
