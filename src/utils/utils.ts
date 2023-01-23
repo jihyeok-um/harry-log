de78afd33483af520940fd767090f54252dd3637
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-24 00:18:49 +0900

    refactor: 성능 문제를 위한 리팩터링

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,10 @@
-import { THUMBNAIL_WIDTH, TRIANGLE_GAP, TRIANGLE_WIDTH } from "./../constants/index";
+import {
+  THUMBNAIL_HEIGHT,
+  THUMBNAIL_WIDTH,
+  TRIANGLE_GAP,
+  TRIANGLE_WIDTH,
+} from "./../constants/index";
+import { Coordinate } from "./../types/index";
 
 export const getCoordinate = (index: number) => {
   const coordinate = {
@@ -8,6 +14,12 @@ export const getCoordinate = (index: number) => {
 
   if (coordinate.x < 0) coordinate.x = 0;
   if (coordinate.y < 0) coordinate.y = 0;
+  if (coordinate.x >= THUMBNAIL_WIDTH) coordinate.x = THUMBNAIL_WIDTH - 1;
+  if (coordinate.y >= THUMBNAIL_HEIGHT) coordinate.y = THUMBNAIL_HEIGHT - 1;
 
   return coordinate;
+};
+
+export const getCurrentPixel = (coordinate: Coordinate) => {
+  const currentPixel = Math.floor(coordinate.x + coordinate.y * THUMBNAIL_WIDTH);
 };
