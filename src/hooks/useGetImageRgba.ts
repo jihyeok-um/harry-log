180c69aaf0b97effac18ab5c1085d9ca29f8425d
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-24 18:55:18 +0900

    refactor: 이미지 rgba 값 가져오는 함수분리

--- src/hooks/useGetImageRgba.ts
+++ src/hooks/useGetImageRgba.ts
@@ -1,32 +1,35 @@
 import { useEffect, useRef, useState } from "react";
-import {
-  RGBA_ARRAY_SIZE,
-  THUMBNAIL_HEIGHT,
-  THUMBNAIL_WIDTH,
-  TRIANGLE_COUNT,
-} from "../constants/triangle";
+import { THUMBNAIL_HEIGHT, THUMBNAIL_WIDTH, TRIANGLE_COUNT } from "../constants/triangle";
+import { RGBA_ARRAY_SIZE } from "./../constants/triangle";
 
-export const useGetImageRgba = () => {
+export const useGetImageRgba = (src: string) => {
   const componentArray = Array.from({ length: TRIANGLE_COUNT }, () => 0);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
   const [rgba, setRgba] = useState<number[][] | null>(null);
+  const image = new Image();
+
+  const createImageData = () => {
+    if (!canvasRef.current || !imageRef.current) return;
+
+    const canvas = canvasRef.current.getContext("2d", { willReadFrequently: true });
+    canvas?.drawImage(imageRef.current, 0, 0, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+
+    return canvas?.getImageData(0, 0, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT).data;
+  };
+
+  const createRgbaArray = (imageData: Uint8ClampedArray | undefined) => {
+    if (!imageData) return;
+
+    const rgbaArray = [];
+    for (let i = 0; i < imageData.length; i += RGBA_ARRAY_SIZE) {
+      rgbaArray.push(Array.from(imageData.slice(i, i + RGBA_ARRAY_SIZE)));
+    }
+    setRgba(rgbaArray);
+  };
 
   useEffect(() => {
-    if (canvasRef.current && imageRef.current) {
-      const canvas = canvasRef.current.getContext("2d", { willReadFrequently: true });
-      canvas?.drawImage(imageRef.current, 0, 0, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
-
-      if (canvas?.getImageData(0, 0, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT)) {
-        const rgbaArray = [];
-        const rgbaData = canvas?.getImageData(0, 0, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT).data;
-
-        for (let i = 0; i < rgbaData.length; i += RGBA_ARRAY_SIZE) {
-          rgbaArray.push(Array.from(rgbaData.slice(i, i + RGBA_ARRAY_SIZE)));
-        }
-        setRgba(rgbaArray);
-      }
-    }
+    createRgbaArray(createImageData());
   }, [canvasRef.current, imageRef.current]);
 
   return {
