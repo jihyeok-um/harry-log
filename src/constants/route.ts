89a54fb5e67b8ba447998db3d3cd4ec4e4495741
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-24 12:37:07 +0900

    feat: BASE_URL 추가

---
+++ src/constants/route.ts
@@ -0,0 +1,7 @@
+const BASE_URL = import.meta.env.BASE_URL;
+console.log(import.meta.env);
+
+export const ROUTE_PATH = {
+  HOME: `${BASE_URL}/`,
+  THUMBNAIL_RESULT: `${BASE_URL}/thumbnail-result`,
+};
