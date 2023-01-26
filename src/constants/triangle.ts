29452bc7958eb632d265893870a84545676eaf39
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-27 03:40:42 +0900

    refactor: 캔버스로 바뀜에 따라 관련 상수 및 공식 수정

--- src/constants/triangle.ts
+++ src/constants/triangle.ts
@@ -1,10 +1,16 @@
 export const THUMBNAIL_WIDTH = 1600;
 export const THUMBNAIL_HEIGHT = 900;
 
-export const TRIANGLE_WIDTH = 16;
-export const TRIANGLE_GAP = 5;
+export const TRIANGLE_SIZE = 40;
+export const TRIANGLE_GAP = 12;
 
-export const TRIANGLE_COUNT =
-  (THUMBNAIL_WIDTH / TRIANGLE_WIDTH) * (THUMBNAIL_HEIGHT / TRIANGLE_WIDTH) * 10;
+export const TRIANGLE_COUNT = (THUMBNAIL_WIDTH / TRIANGLE_GAP) * (THUMBNAIL_HEIGHT / TRIANGLE_GAP);
 
 export const RGBA_ARRAY_SIZE = 4;
+
+//            size | gap
+// 가장 거칠게    60    20
+// 거칠게        50    16
+// 중간         40    12
+// 부드럽게      30    10
+// 가장 부드럽게  20     8
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,29 +1,15 @@
-import {
-  THUMBNAIL_HEIGHT,
-  THUMBNAIL_WIDTH,
-  TRIANGLE_GAP,
-  TRIANGLE_WIDTH,
-} from "../constants/triangle";
+import { THUMBNAIL_WIDTH, TRIANGLE_GAP } from "../constants/triangle";
 import { Coordinate } from "./../types/index";
 
 export const getCoordinate = (index: number) => {
   const coordinate = {
-    x: Math.floor(index % (THUMBNAIL_WIDTH / TRIANGLE_GAP)) * TRIANGLE_WIDTH - TRIANGLE_WIDTH / 2,
-    y: Math.floor(index / (THUMBNAIL_WIDTH / TRIANGLE_GAP)) * TRIANGLE_WIDTH - TRIANGLE_WIDTH / 2,
+    x: Math.floor(index % (THUMBNAIL_WIDTH / TRIANGLE_GAP)) * TRIANGLE_GAP,
+    y: Math.floor(index / (THUMBNAIL_WIDTH / TRIANGLE_GAP)) * TRIANGLE_GAP,
   };
-
-  if (coordinate.x < 0) coordinate.x = 0;
-  if (coordinate.y < 0) coordinate.y = 0;
-  if (coordinate.x >= THUMBNAIL_WIDTH) coordinate.x = THUMBNAIL_WIDTH - 1;
-  if (coordinate.y >= THUMBNAIL_HEIGHT) coordinate.y = THUMBNAIL_HEIGHT - 1;
 
   return coordinate;
 };
 
 export const getCurrentPixel = (coordinate: Coordinate) => {
   return Math.floor(coordinate.x + coordinate.y * THUMBNAIL_WIDTH);
-};
-
-export const randomInt = (min = -TRIANGLE_WIDTH, max = TRIANGLE_WIDTH) => {
-  return Math.floor(Math.random() * (max - min)) + min;
 };
