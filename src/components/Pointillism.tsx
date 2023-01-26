83e7acb884c5901fbda0b936b15ed9aeb12be643
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-01-27 03:41:02 +0900

    feat: 썸네일 다운로드 버튼 구현

--- src/components/Pointillism.tsx
+++ src/components/Pointillism.tsx
@@ -1,35 +1,20 @@
 import styled from "styled-components";
 import { THUMBNAIL_HEIGHT, THUMBNAIL_WIDTH } from "../constants/triangle";
-import { useGetImageRgba } from "../hooks/useGetImageRgba";
+import { usePointillism } from "../hooks/usePointillism";
 import { ThumbnailProps } from "../types";
 
 export const Pointillism = ({ src }: ThumbnailProps) => {
-  const { canvasRef } = useGetImageRgba(src);
+  const { canvasRef } = usePointillism(src);
 
   return (
     <S.Container>
-      <S.ThumbnailContainer>
-        <S.Canvas ref={canvasRef} width={THUMBNAIL_WIDTH} height={THUMBNAIL_HEIGHT}></S.Canvas>
-      </S.ThumbnailContainer>
+      <S.Canvas ref={canvasRef} width={THUMBNAIL_WIDTH} height={THUMBNAIL_HEIGHT}></S.Canvas>
     </S.Container>
   );
 };
 
 const S = {
   Container: styled.div`
-    position: relative;
-  `,
-
-  DownloadButton: styled.a`
-    border: 1px solid #666666;
-    border-radius: 2px;
-    margin-left: 5px;
-    padding: 5px;
-    margin-bottom: 20px;
-  `,
-
-  ThumbnailContainer: styled.div`
-    overflow: hidden;
     position: relative;
     width: 1600px;
     height: 900px;
--- src/pages/ThumbnailResult.tsx
+++ src/pages/ThumbnailResult.tsx
@@ -9,10 +9,25 @@ export const ThumbnailResult = () => {
   return (
     <S.Container>
       <Pointillism src={imageUrl} />
+      <S.DownloadButton>썸네일 다운로드</S.DownloadButton>
     </S.Container>
   );
 };
 
 const S = {
-  Container: styled.div``,
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  `,
+
+  DownloadButton: styled.a`
+    border: 1px solid #666666;
+    border-radius: 2px;
+    margin-left: 5px;
+    padding: 5px;
+    margin-bottom: 20px;
+  `,
 };
