1d612c3807efd1074c2d6fa09e3ac791a143d065
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-02-14 11:12:48 +0900

    Updates

--- assets/index.js
+++ assets/index.js
@@ -260,9 +260,9 @@ Error generating stack: `+i.message+`
     position: relative;
     box-shadow: 2px 2px 2px 2px ${e=>e.theme.GRAY_600};
 
-    @media (min-width: 100rem) {
-      width: 1000px;
-      height: 1000px;
+    @media (min-width: 1600px) {
+      width: 900px;
+      height: 900px;
     }
 
     @media (max-width: 1600px) {
