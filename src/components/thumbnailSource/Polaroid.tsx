59eecc740a0c27ca7795e92963465d5ec4d05914
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2023-03-15 18:24:15 +0900

    feat: 폴라로이드 테두리 내 드래그 할 수 있도록 구현

--- src/components/thumbnailSource/Polaroid.tsx
+++ src/components/thumbnailSource/Polaroid.tsx
@@ -11,12 +11,8 @@ interface PolaroidProps {
 
 const S = {
   Container: styled.img<PolaroidProps>`
-    position: absolute;
-    top: 0px;
-    left: 0px;
     width: 225px;
     height: 300px;
-    box-shadow: 1px 1px 1px 1px ${(props) => props.theme.GRAY_800};
 
     @media (max-width: 600px) {
       width: 150px;
--- src/components/thumbnailSource/RandomPolaroids.tsx
+++ src/components/thumbnailSource/RandomPolaroids.tsx
@@ -1,24 +1,28 @@
 import { motion } from "framer-motion";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { randomInt } from "../../utils/randomInt";
 import { photos } from "../../constants/photos";
 import { Polaroid } from "./Polaroid";
 import { Styles } from "../../styles/Styles";
+import { useRef } from "react";
+import { Coordinate } from "../../types";
 
 export const RandomPolaroids = () => {
+  const boxRef = useRef<HTMLDivElement>(null);
   const width = window.innerWidth;
   const height = window.innerHeight;
 
   return (
-    <S.Container>
+    <S.Container ref={boxRef}>
       {photos.map((photo) => {
         const randomCoord = { x: randomInt(width), y: randomInt(height) };
         const randomRotate = randomInt(45, -45);
 
         return (
-          <motion.div
+          <S.Polaroid
             key={photo}
-            initial={{ scale: 0.8, x: randomCoord.x, y: randomCoord.y, rotate: randomRotate }}
+            randomCoord={randomCoord}
+            initial={{ scale: 0.8, rotateZ: randomRotate }}
             animate={{ scale: 1 }}
             transition={{
               type: "spring",
@@ -26,10 +30,11 @@ export const RandomPolaroids = () => {
               damping: 20,
             }}
             drag
-            dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
+            dragTransition={{ bounceStiffness: 100, bounceDamping: 50 }}
+            dragConstraints={boxRef}
           >
             <Polaroid src={photo} alt="베경 폴라로이드" />
-          </motion.div>
+          </S.Polaroid>
         );
       })}
     </S.Container>
@@ -37,11 +42,32 @@ export const RandomPolaroids = () => {
 };
 
 const S = {
-  Container: styled.div`
+  Container: styled(motion.div)`
     ${Styles.FullWidthAndHeight}
     position: absolute;
     overflow: hidden;
     top: 0;
     left: 0;
+  `,
+
+  Polaroid: styled(motion.div)<{ randomCoord: Coordinate }>`
+    position: absolute;
+    width: 225px;
+    height: 300px;
+    box-shadow: 1px 1px 1px 1px ${(props) => props.theme.GRAY_800};
+
+    @media (max-width: 600px) {
+      width: 150px;
+      height: 200px;
+    }
+
+    ${(props) =>
+      props.randomCoord
+        ? css`
+            position: absolute;
+            top: ${props.randomCoord.y}px;
+            left: ${props.randomCoord.x}px;
+          `
+        : css``}
   `,
 };
